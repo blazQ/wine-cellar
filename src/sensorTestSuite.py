ce3974aaff781cc12dcfd8994a33c2634296ed3a
blazQ <pierngr54@outlook.it>
2023-07-24 19:58:50 +0200

    Slight update to the web app. Added a bit of style and reworked the dashboard. Improved testing script.

--- src/sensorTestSuite.py
+++ src/sensorTestSuite.py
@@ -2,7 +2,8 @@ import sensors
 import boto3
 import random
 import argparse
- 
+import time
+
 from config import DefaultConfig
 
 sqs = boto3.resource('sqs', endpoint_url=DefaultConfig.EXTERNAL_ENDPOINT)
@@ -17,10 +18,11 @@ def massive_script_test(queue_names, error_queue, room_names):
             random_room = random.randint(0, 4)
             current_sensor_type_list.append(queue_names.get(key)(current_queue, error_queue, f"{queue_names.get(key).__name__}-{i}", room_names[random_room], 0.5))
         sensors.append(current_sensor_type_list)
-    
+
     for sensor_type_list in sensors:
         for sensor in sensor_type_list:
             sensor.sense()
+            time.sleep(1)
 
 
 if __name__ == '__main__':
@@ -33,13 +35,13 @@ if __name__ == '__main__':
     config = vars(args)
 
     queue_names =   {
-                        DefaultConfig.DEWPOINT_QUEUE_DEFAULT_NAME: sensors.DewpointSensor, 
-                        DefaultConfig.DOOR_QUEUE_DEFAULT_NAME: sensors.DoorSensor, 
+                        DefaultConfig.DEWPOINT_QUEUE_DEFAULT_NAME: sensors.DewpointSensor,
+                        DefaultConfig.DOOR_QUEUE_DEFAULT_NAME: sensors.DoorSensor,
                         DefaultConfig.VIBRATION_QUEUE_DEFAULT_NAME: sensors.VibrationSensor,
                         DefaultConfig.TEMPERATURE_QUEUE_DEFAULT_NAME: sensors.TemperatureSensor
                     }
     error_queue = sqs.get_queue_by_name(QueueName=DefaultConfig.ERROR_QUEUE_DEFAULT_NAME)
-    
+
     if config['a'] == True:
         room_names = [
             'Sweets',
--- src/webapp/app.rb
+++ src/webapp/app.rb
@@ -10,17 +10,20 @@ API_ENDPOINT = 'http://localhost:4566' # Replace with your LocalStack API endpoi
 
 get '/' do
   query_params = {
-    'room': '*'
+    'room': '*',
+    'door': '*'
   }
+
   api_id = ENV['API_ID']
   # Make a request to your LocalStack API
-  response = HTTParty.get("#{API_ENDPOINT}/restapis/#{api_id}/test/_user_request_/room", query: query_params)
-
-  if response.success?
+  response_rooms = HTTParty.get("#{API_ENDPOINT}/restapis/#{api_id}/test/_user_request_/room", query: query_params)
+  response_doors = HTTParty.get("#{API_ENDPOINT}/restapis/#{api_id}/test/_user_request_/doors", query: query_params)
+  if response_rooms.success? && response_doors.success?
     # Assuming your API returns JSON data
-    data = JSON.parse(response.body)
+    data_rooms = JSON.parse(response_rooms.body)
+    data_doors = JSON.parse(response_doors.body)
     # Do something with the data and render it in your view
-    erb :index, locals: { data: data }
+    erb :index, locals: { data_doors: data_doors, data_rooms: data_rooms }
   else
     "Error: #{response.code}"
   end
